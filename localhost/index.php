961438d5999b40fc8b6b72c2e2f5756b8f0d49ce
ilona <32567245+IlonaBocancea@users.noreply.github.com>
2019-12-09 11:04:31 +0200

    Update index.php

--- localhost/index.php
+++ localhost/index.php
@@ -1,5 +1,4 @@
 <? 
-session_start();
 require 'db.php';
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if (!$connection) {
@@ -7,19 +6,17 @@ if (!$connection) {
     echo "Код ошибки error: " . mysqli_connect_errno() . PHP_EOL;
     echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
     exit;
-
 }
-
 ?>
 <!doctype html>
 <head>
+	<meta charset="UTF-8"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+	<title>Carti online</title>
 <head>
 <body>
 <?
-	
-    include 'blocks/menu.php'?>
+    include 'blocks/menu.php'?>	
 <?  
     $pageName = $_GET['page'];
     $filePath = "{$pageName}.php";
@@ -28,7 +25,7 @@ if (!$connection) {
     }
 	?>
 <?	
-$result = mysqli_query($connection, "SELECT * FROM books");
+//$result = mysqli_query($connection, "SELECT * FROM books");
 ?>
 <form action="" method="post">
 	<input type="text" placeholder="Title" name="titleB">
@@ -36,10 +33,9 @@ $result = mysqli_query($connection, "SELECT * FROM books");
 	<input type="text" placeholder="Year" name="yearB">
 	<input type="submit">
 	<hr>
-	
 </form>
 <?
-	$title = mysqlAdd($connection, $_POST['titleB']);
+	/*$title = mysqlAdd($connection, $_POST['titleB']);
 	$author = mysqlAdd($connection, $_POST['authorB']);
 	$year = mysqlAdd($connection, $_POST['yearB']);
 	$query = "INSERT INTO books(Title,Year) VALUES ('{$title}','{$author}',($year})";
@@ -54,15 +50,43 @@ $result = mysqli_query($connection, "SELECT * FROM books");
 	}
 function mysqlAdd($connect, $title){
 	return htmlentities(mysqli_real_escape_string($connect, $title));
-}
+}*/
 ?>
-<table border="1">
-<? while($books = mysqli_fetch_assoc($result)){?>
-<tr>
-    <td>nr: <?= $books['ID']?>  </td>
-    <td><?= $books['Title']?> </td>
-    <td><?= $books['Year']?></td>
-    <td><?= $books['ID_Author']?></td>
-</tr>
-<? }?>
+<div class="card">
+  <div class="card-header">
+   The most popular books
+  </div>
+  <div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Название карточки</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Название карточки</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Название карточки</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+</div>
 </body>
